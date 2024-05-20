5ea6f41f90aeda7d353dc4120df58e7b2a8a2536
Ruizerick26 <ruierick2525@gmail.com>
2024-05-19 21:30:28 -0500

    correccion

--- src/routers/rutas_moderador.js
+++ src/routers/rutas_moderador.js
@@ -20,7 +20,7 @@ router.post('/login/moderador',login)
 router.post('/moderador/recuperar', recuperaCon)
 router.get('/moderador/recuperar/:token', comprobarRecuperacion)
 router.post('/moderador/nuevopasword/:token', nuevaContraseña)
-router.put('/moderador/contraseñaInicial',contraNuevaI)
+router.put('/moderador/password/inicial',contraNuevaI)
 
 //Rutas privadas
 router.put('/actualizar/moderador/:id',verificarAutentication,actualizarC)
