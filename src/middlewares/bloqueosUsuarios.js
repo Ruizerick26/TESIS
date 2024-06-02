4f10f3a1eb6e1892ca28a1731545dda5de486511
Ruizerick26 <ruierick2525@gmail.com>
2024-06-01 22:14:10 -0500

    correccion

--- src/middlewares/bloqueosUsuarios.js
+++ src/middlewares/bloqueosUsuarios.js
@@ -1,10 +1,10 @@
 import Usuario from "../models/Usuario.js";
 
-const verificarRestrin = async(req,res) =>{
+const verificarRestrin = async(req,res,next) =>{
 
     try{
         const usuarioV = await Usuario.findById(req.usuarioBDD._id).select('-createdAt -updatedAt -__v')
-
+        
         if(usuarioV.restriccion === true) return res.status(200).json({msg:"Su cuenta esta restringida"})
 
         if(usuarioV.restriccion === false) return next()
@@ -14,14 +14,14 @@ const verificarRestrin = async(req,res) =>{
     }
 }
 
-const verificarBloqueo = async (req,res) => {
+const verificarBloqueo = async (req,res,next) => {
     const {email} = req.body
     try{
-        const usuarioV = await Usuario.find({email: email}).select('-createdAt -updatedAt -__v')
+        const usuario = await Usuario.findOne({email})
+        
+        if(usuario.bloqueo === true) return res.status(200).json({msg:"Su cuenta esta bloqueada"})
 
-        if(usuarioV.bloqueo === true) return res.status(200).json({msg:"Su cuenta esta bloqueada"})
-
-        if(usuarioV.bloqueo === false) return next()
+        if(usuario.bloqueo === false) return next()
     }catch(error){
         console.log(error)
         return res.status(404).json({error:error})
